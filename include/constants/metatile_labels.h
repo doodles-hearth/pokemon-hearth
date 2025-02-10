53a32f25a2f87997b5f093f4f70f2ead9d6d8161
RubyRaven6 <swampertspawner@gmail.com>
2025-02-10 13:34:01 +0800

    Changed doors for metatiles, and also fixed beachbound route hole

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -315,6 +315,9 @@
 #define METATILE_LilycoveMuseum_Painting4_Left   0x262
 #define METATILE_LilycoveMuseum_Painting4_Right  0x263
 
+// gTileset_Maguro
+#define METATILE_Maguro_MaguroClosedWindows  0x266
+
 // gTileset_Mauville
 #define METATILE_Mauville_DeepSand_BottomMid  0x259
 #define METATILE_Mauville_DeepSand_Center     0x251
