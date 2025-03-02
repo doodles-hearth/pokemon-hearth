815aa0a9124727d3d1d8a7ce6f5c4d75d4ca583c
purrfectdoodle <purrfectdoodle@gmail.com>
2025-03-02 23:25:06 +0100

    Inline field mugshots!!!

--- include/constants/field_mugshots.h
+++ include/constants/field_mugshots.h
@@ -3,7 +3,9 @@
 
 #define MUGSHOT_NONE  0
 #define MUGSHOT_TEST  1
-#define MUGSHOT_COUNT 2
+#define MUGSHOT_HARIKO  2
+#define MUGSHOT_NATSUKI  3
+#define MUGSHOT_COUNT 4
 
 #define EMOTE_NORMAL  0
 #define EMOTE_ALT     1
--- src/data/field_mugshots.h
+++ src/data/field_mugshots.h
@@ -3,6 +3,12 @@ static const u32 sFieldMugshotGfx_TestAlt[] = INCBIN_U32("graphics/field_mugshot
 static const u16 sFieldMugshotPal_TestNormal[] = INCBIN_U16("graphics/field_mugshots/test/normal.gbapal");
 static const u16 sFieldMugshotPal_TestAlt[] = INCBIN_U16("graphics/field_mugshots/test/alt.gbapal");
 
+static const u32 sFieldMugshotGfx_HarikoNormal[] = INCBIN_U32("graphics/field_mugshots/hariko/normal.4bpp.lz");
+static const u16 sFieldMugshotPal_HarikoNormal[] = INCBIN_U16("graphics/field_mugshots/hariko/normal.gbapal");
+
+static const u32 sFieldMugshotGfx_NatsukiNormal[] = INCBIN_U32("graphics/field_mugshots/natsuki/normal.4bpp.lz");
+static const u16 sFieldMugshotPal_NatsukiNormal[] = INCBIN_U16("graphics/field_mugshots/natsuki/normal.gbapal");
+
 struct MugshotGfx
 {
     const u32 *gfx;
@@ -23,6 +29,22 @@ static const struct MugshotGfx sFieldMugshots[MUGSHOT_COUNT][EMOTE_COUNT] =
         {
             .gfx = sFieldMugshotGfx_TestAlt,
             .pal = sFieldMugshotPal_TestAlt,
+        },
+    },
+    [MUGSHOT_HARIKO] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_HarikoNormal,
+            .pal = sFieldMugshotPal_HarikoNormal,
+        },
+    },
+    [MUGSHOT_NATSUKI] =
+    {
+        [EMOTE_NORMAL] =
+        {
+            .gfx = sFieldMugshotGfx_NatsukiNormal,
+            .pal = sFieldMugshotPal_NatsukiNormal,
         },
     },
 };
