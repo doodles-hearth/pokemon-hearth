38d5584647f2806152af16e37a097bd98b58812d
izrofid <izrofid@gmail.com>
2025-11-17 18:47:28 +0530

    feat: add pkmn avatar bobbing

--- include/player_transform.h
+++ include/player_transform.h
@@ -9,9 +9,12 @@ bool32 IsPlayerTransformed(void);
 void BeginPlayerTransform(void);
 void TransformPlayerToPokemon(void);
 void TransformPlayerToDefault(void);
+void Task_PokemonAvatar_HandleBob(u8 taskId);
 void UpdateTransformedPlayerPalette(struct ObjectEvent *playerObj);
 void CB2_TransformPlayerFromParty(void);
 u16 GetPlayerTransformGfxFromSaveblock();
 u16 SpeciesToGraphicsId(struct Pokemon *pokemon);
+void Task_StartPokemonAvatarBob(u8 taskId);
+void TryStartPokemonAvatarBob(void);
 
 #endif // GUARD_PLAYER_TRANSFORM_H
