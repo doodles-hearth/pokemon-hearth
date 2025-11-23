746c719e148eb3a2dd7e0d3838112667d04c1e21
izrofid <izrofid@gmail.com>
2025-11-23 18:58:49 +0530

    feat: add pkmn avatar bobbing

--- src/player_transform.c
+++ src/player_transform.c
@@ -1,17 +1,41 @@
 #include "global.h"
-#include "player_transform.h"
+#include "script.h"
+#include "field_weather.h"
+#include "field_screen_effect.h"
 #include "field_player_avatar.h"
 #include "event_object_movement.h"
 #include "constants/event_objects.h"
 #include "event_data.h"
 #include "palette.h"
 #include "party_menu.h"
-#include "constants/party_menu.h"
 #include "overworld.h"
-
+#include "constants/party_menu.h"
+#include "player_transform.h"
 static void SetPlayerTransformState(bool32 state);
+static void Task_PartyMenuWaitForFade(u8 taskId);
+static bool8 CB2_FadeFromPartyMenuAfterTransform(void);
 
 struct Pokemon *gPlayerTransformPokemon; // This points to the pokemon the player is currently transformed intos
+
+
+bool8 CB2_FadeFromPartyMenuAfterTransform(void)
+{
+    FadeInFromBlack();
+    CreateTask(Task_PartyMenuWaitForFade, 10);
+    CreateTask(Task_StartPokemonAvatarBob, 9);
+    return TRUE;
+
+}
+
+static void Task_PartyMenuWaitForFade(u8 taskId)
+{
+    if (IsWeatherNotFadingIn())
+    {
+        gTasks[taskId].func = Task_StartPokemonAvatarBob;
+        UnlockPlayerFieldControls();
+        ScriptContext_Enable();
+    }
+}
 
 void CB2_TransformPlayerFromParty(void) // Callback passed to the ChooseMonForTransform function
 {
@@ -28,7 +52,7 @@ void CB2_TransformPlayerFromParty(void) // Callback passed to the ChooseMonForTr
         gPlayerTransformPokemon = &gPlayerParty[gSaveBlock1Ptr->transformMonIndex];
         TransformPlayerToPokemon();
     }
-    gFieldCallback2 = CB2_FadeFromPartyMenu;
+    gFieldCallback2 = CB2_FadeFromPartyMenuAfterTransform;
     SetMainCallback2(CB2_ReturnToField);
     UnfreezeObjectEvents();
 }
@@ -123,3 +147,58 @@ void TransformPlayerToDefault()
     FlagClear(FLAG_DISABLE_FOLLOWERS);
     UpdateFollowingPokemon();
 }
+
+void TryStartPokemonAvatarBob(void)
+{
+    if (!(FuncIsActiveTask(Task_StartPokemonAvatarBob) || FuncIsActiveTask(Task_PokemonAvatar_HandleBob)))
+    {
+        CreateTask(Task_StartPokemonAvatarBob, 10);
+    }
+}
+
+void Task_StartPokemonAvatarBob(u8 taskId)
+{
+    if(gMain.callback2 == CB2_Overworld)
+    {   
+        gTasks[taskId].data[0] = 0;
+        gTasks[taskId].func = Task_PokemonAvatar_HandleBob;
+
+    }
+}
+
+#define STEP_FRAME_DURATION 8
+#define FP_SHIFT 8
+#define FIXED_POINT(n) ((int)((n) * (1 << FP_SHIFT)))
+
+#define tFrameCounter data[0]
+#define tSubpixelOffset data[1]
+void Task_PokemonAvatar_HandleBob(u8 taskId)
+{
+    if (IsPlayerTransformed())
+    {
+        struct Sprite *playerSprite = &gSprites[gObjectEvents[gPlayerAvatar.objectEventId].spriteId];
+        s16 *data = gTasks[taskId].data;
+
+        if (tFrameCounter == 0)
+        {
+            tSubpixelOffset = FIXED_POINT(1); // 1.0 in 8.8 fixed-point
+        }
+
+        if (tFrameCounter == STEP_FRAME_DURATION)
+        {
+            tSubpixelOffset -= FIXED_POINT(0.5); // -0.5
+        }
+
+        if (tFrameCounter == STEP_FRAME_DURATION * 2)
+        {
+            tSubpixelOffset += FIXED_POINT(0.5); // +0.5
+            tFrameCounter = 0;
+            return;
+        }
+
+        playerSprite->y2 = tSubpixelOffset >> FP_SHIFT; // convert to integer for display
+        tFrameCounter++;
+    }
+}
+#undef tFrameCounter
+#undef tSubpixelOffset
