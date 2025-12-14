eabf12ce675de9afbca2a468e26084a46c07f2b8
purrfectdoodle <purrfectdoodle@gmail.com>
2025-12-14 18:39:32 +0100

    Mute logs

--- src/field_screen_effect.c
+++ src/field_screen_effect.c
@@ -1066,8 +1066,8 @@ void WriteFlashScanlineEffectBuffer(u8 flashRadius)
     {
       u32 followSpriteId = GetFollowerObject()->spriteId;
       u32 playerSpriteId = gObjectEvents[GetObjectEventIdByLocalIdAndMap(LOCALID_PLAYER, 0, 0)].spriteId;
-      DebugPrintf("PlayerSpriteId: %u", playerSpriteId);
-      DebugPrintf("FollowerSpriteId: %u", followSpriteId);
+      /* DebugPrintf("PlayerSpriteId: %u", playerSpriteId); */
+      /* DebugPrintf("FollowerSpriteId: %u", followSpriteId); */
 
       u32 species;
       u32 level;
