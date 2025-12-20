5e2ec003b8c995c09bf79387a01a276bc87ca44f
Kildemal <kildemal@protonmail.com>
2025-12-20 19:31:14 +0530

    feat(battle): add animation to smoke weather explosion
    Uses the explosion move animation

--- include/battle_anim_scripts.h
+++ include/battle_anim_scripts.h
@@ -1007,6 +1007,8 @@ extern const u8 gBattleAnimGeneral_TeraCharge[];
 extern const u8 gBattleAnimGeneral_TeraActivate[];
 extern const u8 gBattleAnimGeneral_SimpleHeal[];
 extern const u8 gBattleAnimGeneral_PowerConstruct[];
+extern const u8 gBattleAnimGeneral_Explosion[];
+
 
 // special animations
 extern const u8 gBattleAnimSpecial_LevelUp[];
