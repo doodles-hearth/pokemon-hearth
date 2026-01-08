0c57c67712b21b14356d6a7af9db95f683b47d8f
Kildemal <kildemal@protonmail.com>
2026-01-08 21:04:56 +0530

    feat(graphics): add macros to reduce spot code boilerplate

--- src/data/pokemon_graphics/pokemon_spot_templates.h
+++ src/data/pokemon_graphics/pokemon_spot_templates.h
@@ -2,57 +2,66 @@
 #define GUARD_POKEMON_SPOT_TEMPLATES_H
 
 #include "global.h"
+#include "metaprogram.h"
 #include "pokemon_spots.h"
+#include "repeat_macros.h"
+
+#define GEN_SPOT_IMAGES(tag, idx, size)                                                            \
+    INCBIN_U##size(TO_LOWER("graphics/pokemon/" STR(tag) "/spots/spot_" STR(idx) ".1bpp")),
+
+#define GEN_SPOT_IMAGES_16(tag, idx) GEN_SPOT_IMAGES(tag, idx, 16)
+#define GEN_SPOT_IMAGES_32(tag, idx) GEN_SPOT_IMAGES(tag, idx, 32)
+
+#define SPOT_ARRAY_16(name,  count) \
+    static const SpotRow16 s##name##SpotImages[count] = { \
+    RECURSIVELY(R_ZIP(GEN_SPOT_IMAGES_16, REPEAT_N(count, name), NUMBERS_16)) \
+    }
+
+#define SPOT_ARRAY_32(name,  count) \
+    static const SpotRow32 s##name##SpotImages[count] = { \
+    RECURSIVELY(R_ZIP(GEN_SPOT_IMAGES_32, REPEAT_N(count, name), NUMBERS_16)) \
+    }
+
+#define MON_SPOT_16(name, idx, _x, _y) \
+    { .x = _x, .y = _y, .image.rows16 = s##name##SpotImages[idx] },
+
+#define MON_SPOT_32(name, idx, _x, _y) \
+    { .x = _x, .y = _y, .image.rows32 = s##name##SpotImages[idx] },
+
+#define MON_SPOT_TEMPLATE(name, _scale, _x, _y, first, last, adjust) \
+    const struct MonSpotTemplate g##name##SpotTemplate = { \
+        .spots = s##name##Spots, \
+        .count = ARRAY_COUNT(s##name##Spots), \
+        .scale = _scale, \
+        .xOffsetFrame2 = _x, \
+        .yOffsetFrame2 = _y, \
+        .firstColor = first, \
+        .lastColor = last, \
+        .colorAdjust = adjust, \
+    };
 
 typedef u16 SpotRow16[16];
 typedef u32 SpotRow32[32];
 
-static const SpotRow16 sSpindaSpotImages[4] = {
-    INCBIN_U16("graphics/pokemon/spinda/spots/spot_0.1bpp"),
-    INCBIN_U16("graphics/pokemon/spinda/spots/spot_1.1bpp"),
-    INCBIN_U16("graphics/pokemon/spinda/spots/spot_2.1bpp"),
-    INCBIN_U16("graphics/pokemon/spinda/spots/spot_3.1bpp")};
+/* Define Spots Below*/
 
+SPOT_ARRAY_16(Spinda, 4);
 static const struct MonSpot sSpindaSpots[] = {
-    { .x = 16, .y = 7,  .image.rows16 = sSpindaSpotImages[0] },
-    { .x = 40, .y = 8,  .image.rows16 = sSpindaSpotImages[1] },
-    { .x = 22, .y = 25, .image.rows16 = sSpindaSpotImages[2] },
-    { .x = 34, .y = 26, .image.rows16 = sSpindaSpotImages[3] },
+MON_SPOT_16(Spinda, 0, 16, 7)
+MON_SPOT_16(Spinda, 1, 40, 8)
+MON_SPOT_16(Spinda, 2, 22, 27)
+MON_SPOT_16(Spinda, 3, 34, 26)
 };
+MON_SPOT_TEMPLATE(Spinda, 1, -4, 0, 1, 3, 4)
 
-const struct MonSpotTemplate gSpindaSpotTemplate = {
-    .spots = sSpindaSpots,
-    .count = ARRAY_COUNT(sSpindaSpots),
-    .scale = 1,
-    .xOffsetFrame2 = -4,
-    .yOffsetFrame2 = 0,
-    .firstColor = 1,
-    .lastColor = 3,
-    .colorAdjust = 4,
-};
 
-static const SpotRow32 sGyaradosSpotImages[4] = {
-    INCBIN_U32("graphics/pokemon/gyarados/spots/spot_0.1bpp"),
-    INCBIN_U32("graphics/pokemon/gyarados/spots/spot_1.1bpp"),
-    INCBIN_U32("graphics/pokemon/gyarados/spots/spot_2.1bpp"),
-    INCBIN_U32("graphics/pokemon/gyarados/spots/spot_3.1bpp")};
-
+SPOT_ARRAY_32(Gyarados, 4);
 static const struct MonSpot sGyaradosSpots[] = {
-    { .x = 16, .y = 32, .image.rows32 = sGyaradosSpotImages[0] },
-    { .x = 32, .y = 8, .image.rows32 = sGyaradosSpotImages[1] },
-    { .x = 54, .y = 40, .image.rows32 = sGyaradosSpotImages[2] },
-    { .x = 34, .y = 26, .image.rows32 = sGyaradosSpotImages[3] },
+    MON_SPOT_32(Gyarados, 0, 16, 32)
+    MON_SPOT_32(Gyarados, 1, 32, 8)
+    MON_SPOT_32(Gyarados, 2, 54, 40)
+    MON_SPOT_32(Gyarados, 3, 26, 26)
 };
-
-const struct MonSpotTemplate gGyaradosSpotTemplate = {
-    .spots = sGyaradosSpots,
-    .count = ARRAY_COUNT(sGyaradosSpots),
-    .scale = 2,
-    .xOffsetFrame2 = 4,
-    .yOffsetFrame2 = 8,
-    .firstColor = 8,
-    .lastColor = 9,
-    .colorAdjust = 2,
-};
+MON_SPOT_TEMPLATE(Gyarados, 2, 4, 8, 8, 9, 2)
 
 #endif /* end of include guard: GUARD_POKEMON_SPOT_TEMPLATES_H */
