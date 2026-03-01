b909a40fb9dc8ad0f1d93aa664a58d7d116ab62c
Kildemal <kildemal@protonmail.com>
2026-03-01 16:31:36 +0530

    fix(test): wrong config name in ai switching test

--- test/battle/ai/ai_switching.c
+++ test/battle/ai/ai_switching.c
@@ -2195,7 +2195,7 @@ AI_SINGLE_BATTLE_TEST("AI_FLAG_RANDOMIZE_SWITCHIN: AI will consider all mons tha
 AI_MULTI_BATTLE_TEST("AI will not switch out if the opposite battler is absent and its moves can still affect the other opponent")
 {
     GIVEN {
-        WITH_CONFIG(CONFIG_MULTI_BATTLE_WHITEOUT, GEN_LATEST);
+        WITH_CONFIG(B_MULTI_BATTLE_WHITEOUT, GEN_LATEST);
         AI_FLAGS(AI_FLAG_CHECK_BAD_MOVE | AI_FLAG_CHECK_VIABILITY | AI_FLAG_TRY_TO_FAINT);
         MULTI_PLAYER(SPECIES_WOBBUFFET) { HP(41); Speed(1); }
         MULTI_PARTNER(SPECIES_DRAKLOAK) { HP(41); MaxHP(100); Speed(4); Item(ITEM_LIFE_ORB); Moves(MOVE_DRAGON_RAGE, MOVE_SHADOW_BALL); }
