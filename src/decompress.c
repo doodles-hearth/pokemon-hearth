f0d14bb6ff8f30f39bb33a34359153e431aa2b1b
Jamie Foster <jamiefosterandco@gmail.com>
2026-04-13 12:45:43 +0200

    Add diverse eggs based on egg groups and types to Hearth

--- src/decompress.c
+++ src/decompress.c
@@ -3,6 +3,7 @@
 #include "data.h"
 #include "decompress.h"
 #include "decompress_error_handler.h"
+#include "diverse_eggs.h"
 #include "pokemon.h"
 #include "pokemon_spots.h"
 #include "pokemon_sprite_visualizer.h"
@@ -1139,9 +1140,14 @@ void LoadSpecialPokePicIsEgg(void *dest, enum Species species, u32 personality, 
     if (isEgg)
     {
         if (gSpeciesInfo[species].eggId != EGG_ID_NONE)
+        {
             DecompressDataWithHeaderWram(gEggDatas[gSpeciesInfo[species].eggId].eggSprite, dest);
+        }
         else
-            DecompressDataWithHeaderWram(gSpeciesInfo[SPECIES_EGG].frontPic, dest);
+        {
+            const u32 *spriteData = GetEggSpriteData(species);
+            DecompressDataWithHeaderWram(spriteData, dest);
+        }
     }
     else if (isFrontPic)
     {
