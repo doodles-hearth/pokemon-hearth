5067ddad443fb76eb20fcc190444a17556d81f48
Jamie Foster <jamiefosterandco@gmail.com>
2026-05-19 14:00:20 +0200

    daily events debug

--- src/clock.c
+++ src/clock.c
@@ -46,6 +46,27 @@ static void UpdateDailySeed(void)
     gSaveBlock1Ptr->dailySeed = Random32();
 }
 
+void DoDailyEvents(u32 daysSince)
+{
+    ClearDailyFlags();
+    ClearDailyTrainers();
+    UpdateDailySeed();
+    UpdateShamishenPlayersLocations();
+    UpdateDewfordTrendPerDay(daysSince);
+    UpdateTVShowsPerDay(daysSince);
+    UpdateWeatherPerDay(daysSince);
+    UpdatePartyPokerusTime(daysSince);
+    UpdateMirageRnd(daysSince);
+    UpdateBirchState(daysSince);
+    UpdateFrontierManiac(daysSince);
+    UpdateFrontierGambler(daysSince);
+    SetShoalItemFlag(daysSince);
+    SetRandomLotteryNumber(daysSince);
+    UpdateDaysPassedSinceFormChange(daysSince);
+    RollDailyCampfireEvents(daysSince);
+    DailyResetApricornTrees();
+}
+
 static void UpdatePerDay(struct Time *localTime)
 {
     u16 *days = GetVarPointer(VAR_DAYS);
@@ -54,23 +75,7 @@ static void UpdatePerDay(struct Time *localTime)
     if (*days != localTime->days && *days <= localTime->days)
     {
         daysSince = localTime->days - *days;
-        ClearDailyFlags();
-        ClearDailyTrainers();
-        UpdateDailySeed();
-        UpdateShamishenPlayersLocations();
-        UpdateDewfordTrendPerDay(daysSince);
-        UpdateTVShowsPerDay(daysSince);
-        UpdateWeatherPerDay(daysSince);
-        UpdatePartyPokerusTime(daysSince);
-        UpdateMirageRnd(daysSince);
-        UpdateBirchState(daysSince);
-        UpdateFrontierManiac(daysSince);
-        UpdateFrontierGambler(daysSince);
-        SetShoalItemFlag(daysSince);
-        SetRandomLotteryNumber(daysSince);
-        UpdateDaysPassedSinceFormChange(daysSince);
-        RollDailyCampfireEvents(daysSince);
-        DailyResetApricornTrees();
+        DoDailyEvents(daysSince);
         *days = localTime->days;
     }
 }
